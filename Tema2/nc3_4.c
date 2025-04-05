cc9517957760f86dd601c2e8a6ca006c94a8c3cb
186rodrigo <186rodrigo@gmail.com>
2025-04-05 18:04:35 +0000

    Dia 05-04-2025

---
+++ Tema2/nc3_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int nota = 60;
+    if (nota >= 60)
+    { printf("Parabens, você passou.\n");
+    } else
+    { printf("Infelizmente, você não passou.\n");
+    }
+    
+}
